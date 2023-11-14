c0729fc7d0bf820d3259609781b64a1f5c2d9137
Sören Metje <soerenmetje@yahoo.de>
2023-11-14 21:13:37 +0100

    Set number of files to 128 in sysbench fileio sequential Set blocksize to 256K sysbench fileio sequential and randomized

--- src/benchmark/slurm/workload-sysbench-diskrnd.sh
+++ src/benchmark/slurm/workload-sysbench-diskrnd.sh
@@ -10,6 +10,6 @@ cd tmp || exit 1
 
 sysbench fileio --file-total-size=1G --file-num=128 prepare
 # --file-extra-flags=direct => the file reading and writing mode is changed to direct. FileIO is done directly from user space buffers. See O_DIRECT: https://www.man7.org/linux/man-pages/man2/open.2.html
-sysbench fileio --file-total-size=1G --file-num=128 --file-test-mode=rndrd --max-requests=0 --file-extra-flags=direct run
-sysbench fileio --file-total-size=1G --file-num=128 --file-test-mode=rndwr --max-requests=0 --file-extra-flags=direct run
+sysbench fileio --file-total-size=1G --file-num=128 --file-test-mode=rndrd --file-block-size=256K --file-extra-flags=direct run
+sysbench fileio --file-total-size=1G --file-num=128 --file-test-mode=rndwr --file-block-size=256K --file-extra-flags=direct run
 sysbench fileio cleanup
--- src/benchmark/slurm/workload-sysbench-diskseq.sh
+++ src/benchmark/slurm/workload-sysbench-diskseq.sh
@@ -7,8 +7,8 @@ cd /nfs/workloads/slurm || exit 1
 mkdir -p tmp
 cd tmp || exit 1
 
-sysbench fileio --file-total-size=1G --file-num=4 prepare
+sysbench fileio --file-total-size=1G --file-num=128 prepare
 # --file-extra-flags=direct => the file reading and writing mode is changed to direct. FileIO is done directly from user space buffers. See O_DIRECT: https://www.man7.org/linux/man-pages/man2/open.2.html
-sysbench fileio --file-total-size=1G --file-num=4 --file-test-mode=seqrd --file-extra-flags=direct run
-sysbench fileio --file-total-size=1G --file-num=4 --file-test-mode=seqwr --file-extra-flags=direct run
+sysbench fileio --file-total-size=1G --file-num=128 --file-test-mode=seqrd --file-block-size=256K --file-extra-flags=direct run
+sysbench fileio --file-total-size=1G --file-num=128 --file-test-mode=seqwr --file-block-size=256K --file-extra-flags=direct run
 sysbench fileio cleanup
